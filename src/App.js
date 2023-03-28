e29ddcf9e23973161b790b40a776f555eeeaa281
Anders Bogan <adbogan@cox.net>
2023-03-28 11:35:00 -0700

    made API key public (it's free.. eh)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css'
 import axios from 'axios'
 import CityName from './components/CityName/CityName'
-import Config from './Config.json'
 import React from 'react'
 import Temperature from './components/Temperature/Temperature'
 import WeatherGraphic from './components/WeatherGraphic/WeatherGraphic'
@@ -33,7 +32,7 @@ class App extends React.Component {
       url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
       params: {q: this.state.input, days: '1'},
       headers: {
-        'X-RapidAPI-Key': Config.API_KEY,
+        'X-RapidAPI-Key': '11dfc2446emsh483e7b19f8f92c2p15d69fjsn08addc5ee5b1',
         'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
       }
     };
